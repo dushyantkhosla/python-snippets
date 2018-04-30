369c1e164f0b5fd119ae19a09356ba8ed5a0d8ce
Dushyant Khosla <dushyant.khosla@yahoo.com>
2018-04-30 14:23:08 +0200

    Update __init__.py

--- scrub/__init__.py
+++ scrub/__init__.py
@@ -1,4 +1,4 @@
 from .clip import clip_categorical
 from .compress import compress_numeric, compress_categorical
 from .dummies import make_dummies
-from .clean import clean_zv, clean_missing
+from .clean import drop_zv, drop_missings
