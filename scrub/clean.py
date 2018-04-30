1a90bf54f43baaf0133d8a4c628091e1cbae1ba8
Dushyant Khosla <dushyant.khosla@yahoo.com>
2018-04-30 15:25:07 +0200

    Update clean.py

--- scrub/clean.py
+++ scrub/clean.py
@@ -36,6 +36,25 @@ def drop_zv(df_):
         print("No columns with zero-variance.")
     return df_
 
+def drop_nzv(df_, nzv_threshold=0.95):
+    """
+    """
+    cols_catg_nzv = \
+    (df_
+     .select_dtypes(include='object')
+     .apply(lambda c: c.value_counts(normalize=True).agg(['max', 'idxmax']))
+     .T
+     .query("max > {}".formata(nzv_threshold))
+     .index
+     .tolist()
+    )
+
+    if len(cols_catg_nzv) >= 1:
+        print("The mode of these columns has a frequency higher than {}. Dropping these. {}"
+              .format(nzv_threshold, cols_catg_nzv))
+        df_.drop(cols_catg_nzv, axis=1, inplace=True)
+    else:
+        print("No categorical columns with near-zero variance found.")
 
 def drop_missings(df_, threshold=0.8):
     """
